469e0eca45dce23fa55fd7b052bf2b4d92e872ed
Pedro Leal <pedrombl@gmail.com>
2013-09-03 21:02:53 -0300

    refatorando lista-valor-palavras

--- src/user_score/core.clj
+++ src/user_score/core.clj
@@ -5,16 +5,16 @@
 (defn letra->peso [letra]
   (- (int letra) peso-base))
 
-(defn calcula-peso-palavra
-  [palavra]
+(defn calcula-peso-palavra [palavra]
   (reduce + (map letra->peso palavra)))
   
   
 (defn ordena-lista-de-palavras [lista]
   (sort lista))
 
+(defn peso-ponderado [posicao palavra]
+  (* (calcula-peso-palavra palavra) (inc posicao)))
+
 (defn lista-valor-palavras [lista]
-   (map-indexed 
-     (fn [posicao palavra] (* (calcula-peso-palavra palavra) (inc posicao)))
-     lista))
+   (map-indexed peso-ponderado lista))
 
